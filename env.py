d7ed7df40cc3d0a30cf364436291a5d633fbd924
Gabriel Cavazos <gabecavazos3@gmail.com>
2023-05-16 13:44:16 -0500

    env file to store github info

---
+++ env.py
@@ -0,0 +1,10 @@
+# Make a github personal access token.
+#     1. Go here and generate a personal access token https://github.com/settings/tokens
+#        You do _not_ need select any scopes, i.e. leave all the checkboxes unchecked
+#     2. Save it in your env.py file under the variable `github_token`
+# Add your github username to below under the variable `github_username`
+# Add more repositories to the `REPOS` list below.
+
+github_token=''
+
+github_username=''
